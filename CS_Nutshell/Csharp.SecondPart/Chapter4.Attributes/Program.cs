4a146d9210aefd2008ffcf44a0fb378cdfc5a1ef
vincent tene <vincent.tene@gmail.com>
2021-04-19 16:03:58 +0200

    new project added

---
+++ CS_Nutshell/Csharp.SecondPart/Chapter4.Attributes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+
+namespace Chapter4.Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Foo();   // Generates a warning because Foo is obsolete
+
+
+            FooCall();
+
+
+            Console.ReadKey();
+        }
+
+        static void FooCall(
+  [CallerMemberName] string memberName = null,
+  [CallerFilePath] string filePath = null,
+  [CallerLineNumber] int lineNumber = 0)
+        {
+            Console.WriteLine(memberName);
+            Console.WriteLine(filePath);
+            Console.WriteLine(lineNumber);
+        }
+
+
+
+    }
+
+    [XmlType("Customer", Namespace = "http://oreilly.com")]
+    public class CustomerEntity
+    {
+    }
+
+
+
+    [Obsolete]
+    public class Foo
+    {
+    }
+
+
+}
---
+++ CS_Nutshell/Csharp.SecondPart/Chapter4.DynamicBindings/Duck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Chapter4.DynamicBindings
+{
+
+    public class Duck : System.Dynamic.DynamicObject
+    {
+        public override bool TryInvokeMember(
+          InvokeMemberBinder binder, object[] args, out object result)
+        {
+            Console.WriteLine(binder.Name + " method was called");
+            result = null;
+            return true;
+        }
+    }
+}
