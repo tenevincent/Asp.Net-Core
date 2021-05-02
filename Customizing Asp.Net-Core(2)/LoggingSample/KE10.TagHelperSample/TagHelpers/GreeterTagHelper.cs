025ff9c574a9569e3d44225d8c6bc59edb7a4038
Tene Kengne Vincent De Paul <vincent.tene@gmail.com>
2021-05-02 23:15:49 +0200

    new project added

---
+++ Customizing Asp.Net-Core(2)/LoggingSample/KE10.TagHelperSample/TagHelpers/GreeterTagHelper.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace KEB10.TagHelperSample.TagHelpers
+{
+    public class DataGridTagHelper : TagHelper
+    {
+        [HtmlAttributeName("Items")]
+        public IEnumerable<object> Items { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "table";
+            output.Attributes.Add("class", "table");
+            var props = GetItemProperties();
+            TableHeader(output, props);
+            TableBody(output, props);
+        }
+
+        private PropertyInfo[] GetItemProperties()
+        {
+            var listType = Items.GetType();
+            Type itemType;
+            if (listType.IsGenericType)
+            {
+                itemType = listType.GetGenericArguments()
+                .First();
+                return itemType.GetProperties(
+                BindingFlags.Public |
+                BindingFlags.Instance);
+            }
+            return new PropertyInfo[] { };
+        }
+
+        private void TableHeader(TagHelperOutput output, PropertyInfo[] props)
+        {
+            output.Content.AppendHtml("<thead>");
+            output.Content.AppendHtml("<tr>");
+            foreach (var prop in props)
+            {
+                var name = GetPropertyName(prop);
+                output.Content.AppendHtml($"<th>{name}</th>");
+                output.Content.AppendHtml("</tr>");
+                output.Content.AppendHtml("</thead>");
+            }
+        }
+
+
+        private string GetPropertyName(PropertyInfo property)
+        {
+            var attribute = property
+            .GetCustomAttribute<DisplayNameAttribute>();
+            if (attribute != null)
+            {
+                return attribute.DisplayName;
+            }
+            return property.Name;
+        }
+
+        private void TableBody(TagHelperOutput output, PropertyInfo[] props)
+        {
+            output.Content.AppendHtml("<tbody>");
+            foreach (var item in Items)
+            {
+                output.Content.AppendHtml("<tr>");
+                foreach (var prop in props)
+                {
+                    var value = GetPropertyValue(prop, item);
+                    output.Content.AppendHtml($"<td>{value}</td>");
+                }
+                output.Content.AppendHtml("</tr>");
+            }
+            output.Content.AppendHtml("</tbody>");
+        }
+
+
+        private object GetPropertyValue(PropertyInfo property, object instance)
+        {
+            return property.GetValue(instance);
+        }
+    }
+
+}
---
+++ Customizing Asp.Net-Core(2)/LoggingSample/KE90.ActionFilterSample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using KE9.ActionFilterSample.Models;
+using KE9.ActionFilterSample.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace KE9.ActionFilterSample.Controllers
+{
+
+    [ServiceFilter(typeof(LoggingActionFilter))]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
